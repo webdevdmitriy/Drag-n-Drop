008e737dda10a95cb4e68a9872480509973cb95e
Dmitriy <plotnikovdi95@gmail.com>
2022-06-23 09:55:48 +0300

    Добавлено удаление элементов из зон, связей и ос. Улучшен код

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
-const shelf = document.querySelector('.shelf')
+const shelfs = document.querySelectorAll('.shelf [data-shelf]')
 const zone = document.querySelector('.zone ')
-const zones = document.querySelectorAll('.zone div')
+const zones = document.querySelectorAll('.zone div[data-zone]')
 let dragsItems = document.querySelectorAll('.dragItem')
 
 //отменяем запрет перетаскивания элементов в другие элементы
-
 zones.forEach(item => (item.ondragover = e => e.preventDefault()))
+shelfs.forEach(item => (item.ondragover = e => e.preventDefault()))
 
 // Перетаскиваемые объекты
 dragsItems.forEach(item => {
@@ -15,7 +15,7 @@ dragsItems.forEach(item => {
 
 // Зоны для перетаскивания объектов
 zones.forEach(dropZone => {
-  dropZone.addEventListener('drop', handlerDrop)
+  dropZone.addEventListener('drop', handlerDropZone)
   dropZone.addEventListener('dragenter', handlerDragEnter)
   dropZone.addEventListener('dragleave', handlerDragLeave)
 })
@@ -29,42 +29,59 @@ function handlerDragStart(e) {
   zones.forEach(item => {
     if (item.dataset.zone != zoneId) item.classList.add('dark-background')
   })
+
+  // Изменение z index у svg, чтобы svg ушла на второй план и можно было переместить элемент
+  document.querySelector('svg').style.zIndex = '0'
 }
 
 function handlerDragEnd(e) {
   zones.forEach(item => {
     item.classList.remove('dark-background')
   })
+  document.querySelector('svg').style.zIndex = '1'
 }
 
-function handlerDrop(e) {
+function handlerDropZone(e) {
   let item = e.dataTransfer.getData('item')
   let zoneId = e.dataTransfer.getData('zone')
   let dragItem = document.querySelector(`[data-item="${item}"]`)
   let itemInZone = this.querySelector('div')
 
-  if (this.dataset.zone == zoneId) {
+  // Для блоков
+  if (!dragItem.classList.contains('signal') && this.dataset.zone == zoneId) {
     itemInZone && document.querySelector('.shelf-' + zoneId).append(itemInZone)
     this.append(dragItem)
   }
-  document.querySelector('.zone-' + zoneId).classList.remove('dropZone--active')
+  // Для сигналов
+  if (dragItem.classList.contains('signal') && this.dataset.zone == zoneId) {
+    this.querySelector('.signals').append(dragItem)
+    const signal_block = document.querySelector(`[data-signalBlock="${dragItem.dataset.signal}"]`)
+    signal_block.style.display = 'flex'
+  }
 
   let itemInZone1
   let itemInZone2
   if (zoneId >= 2) {
     itemInZone1 = document.querySelector(`.zone-${zoneId - 1}`)
+    if (!itemInZone1.classList.contains('zone-drop')) {
+      itemInZone1 = itemInZone1.querySelector('.zone-drop')
+    }
   }
 
   if (zoneId < zones.length) {
     itemInZone2 = document.querySelector(`.zone-${Number(zoneId) + 1}`)
+    if (!itemInZone2.classList.contains('zone-drop')) {
+      itemInZone2 = itemInZone2.querySelector('.zone-drop')
+      console.log(itemInZone2)
+    }
   }
 
   if (itemInZone1 && itemInZone1.hasChildNodes()) {
-    drawLine(itemInZone1.querySelector('div'), dragItem)
+    drawLine(itemInZone1.querySelector('div.dragItem'), dragItem)
   }
 
   if (itemInZone2 && itemInZone2.hasChildNodes()) {
-    drawLine(dragItem, itemInZone2.querySelector('div'))
+    drawLine(dragItem, itemInZone2.querySelector('div.dragItem'))
   }
   showDataSignal()
 }
@@ -74,6 +91,27 @@ function handlerDragEnter(e) {
 }
 function handlerDragLeave() {}
 
+// Перетаскивание блоков обратно на полку
+shelfs.forEach(dropZone => {
+  dropZone.addEventListener('drop', handlerDropShelf)
+})
+
+function handlerDropShelf(e) {
+  let item = e.dataTransfer.getData('item')
+  let zoneId = e.dataTransfer.getData('zone')
+  let dragItem = document.querySelector(`[data-item="${item}"]`)
+
+  if (this.dataset.shelf == zoneId) {
+    this.querySelector('.shelf__block').append(dragItem)
+  }
+
+  // Удаление линий
+  document.querySelector(`[data-line="${Number(zoneId)}-${Number(zoneId) + 1}"]`) && document.querySelector(`[data-line="${Number(zoneId)}-${Number(zoneId) + 1}"]`).remove()
+  document.querySelector(`[data-line="${Number(zoneId) - 1}-${Number(zoneId)}"]`) && document.querySelector(`[data-line="${Number(zoneId) - 1}-${Number(zoneId)}"]`).remove()
+
+  // Удаление инпутов сигналов
+  dragItem.dataset.signal && (document.querySelector(`[data-signalBlock="${dragItem.dataset.signal}"]`).style.display = 'none')
+}
 //===================================================================================
 // Рисуем линию
 
@@ -81,8 +119,6 @@ let zoneY = zone.getBoundingClientRect().top
 let zoneX = zone.getBoundingClientRect().left
 
 function drawLine(item1, item2) {
-  console.log(item1)
-  console.log(item2)
   let react1 = item1 && item1.getBoundingClientRect()
   let react2 = item2 && item2.getBoundingClientRect()
 
@@ -90,10 +126,11 @@ function drawLine(item1, item2) {
   line.style.stroke = 'rgb(255, 0, 0)'
   line.style.strokeWidth = '2'
 
-  line.setAttribute('x1', react1.left - zoneX)
+  line.setAttribute('x1', react1.left - zoneX + 5)
   line.setAttribute('y1', react1.top - zoneY + react1.height - 10)
-  line.setAttribute('x2', react2.left - zoneX)
+  line.setAttribute('x2', react2.left - zoneX + 5)
   line.setAttribute('y2', react2.top - zoneY - 10)
+  line.dataset.line = `${item1.dataset.zone}-${item2.dataset.zone}`
 
   let svg = document.querySelector('svg')
   svg.append(line)
@@ -120,11 +157,22 @@ function drawLineFeedBack(block1, block2) {
   line.setAttribute('points', points)
   let svg = document.querySelector('svg')
   svg.append(line)
+
+  let polylines = document.querySelectorAll('polyline')
+
+  polylines.forEach(item => {
+    console.log(12)
+    item.addEventListener('click', function () {
+      console.log(23)
+      this.remove()
+    })
+  })
 }
 
 //================================================================
 // Добавление входов и выходов для каждого блока
-dragsItems.forEach(function (item) {
+let dragsItemsWithoutSignals = document.querySelectorAll('.dragItem:not(.signal)')
+dragsItemsWithoutSignals.forEach(function (item) {
   let enter1 = document.createElement('span')
   let enter2 = document.createElement('span')
   let out1 = document.createElement('span')
@@ -158,17 +206,17 @@ outs2.forEach(function (item) {
 //========================================================
 // выбор сигнала
 
-const dataSignalSelect = document.querySelector('.data-signal_select')
-const dataSignalInput = document.querySelector('.data-signal_input')
+const dataSignalSelects = document.querySelectorAll('.signals-data__select')
 
-dataSignalSelect.addEventListener('change', function () {
-  dataSignalInput.value = this.value
-  if (this.value != 0) document.querySelector('.point').style.backgroundColor = 'green'
-  else document.querySelector('.point').style.backgroundColor = 'red'
+dataSignalSelects.forEach(item => {
+  item.addEventListener('change', function () {
+    let signalBlock = this.closest('[data-signalblock]')
+
+    signalBlock.querySelector('input').value = this.value
+  })
 })
-
 function showDataSignal() {
-  if (document.querySelector('.zone-1').hasChildNodes()) {
-    document.querySelector('.data-signal_block').style.display = 'block'
-  }
+  // if (document.querySelector('.zone-1').hasChildNodes()) {
+  //   document.querySelector('.data-signal_block').style.display = 'block'
+  // }
 }
